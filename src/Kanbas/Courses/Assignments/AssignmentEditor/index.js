f550020422e49c64f736d8d8a29b0234ad336cce
hrishikasamani <samani.hr@northeastern.edu>
2023-10-25 06:29:39 -0400

    tiny changes

--- src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
+++ src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import db from "../../../Database";
-import "./index.css";
-
+import "./index2.css";
+import {BiDotsVerticalRounded} from "react-icons/bi";
+import {AiFillCheckCircle} from "react-icons/ai";
 
 function AssignmentEditor() {
   const { assignmentId } = useParams();
@@ -17,6 +18,18 @@ function AssignmentEditor() {
     navigate(`/Kanbas/Courses/${courseId}/Assignments`);
   };
   return (
+
+<div className="wd-flex-row-container">
+  <div className="wd-flex">
+            <button class="btn-clear" disabled>
+              <AiFillCheckCircle/>
+                  Published
+                </button>
+            <button class="btn">
+            <BiDotsVerticalRounded/>
+            </button>
+  </div> 
+        <hr />
     <div className="assignment">
       <h5>Assignment Name</h5>
       <input value={assignment.title}
@@ -24,13 +37,14 @@ function AssignmentEditor() {
         <hr />
       <div className="wd-flex">   
       <Link to={`/Kanbas/Courses/${courseId}/Assignments`}
-            className="btn btn-danger">
+            className="btn">
         Cancel
       </Link>
-      <button onClick={handleSave} className="btn btn-success">
+      <button onClick={handleSave} className="btn btn-danger">
         Save
       </button>
       </div> 
+    </div>
     </div>
   );
 }
--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,15 +1,40 @@
 import React from "react";
+import AssignmentList from "./AssignmentList/AssignmentList";
 import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
+import {AiOutlinePlus} from "react-icons/ai"
+import {AiOutlineCheckCircle} from "react-icons/ai"
+import {BiDotsVerticalRounded} from "react-icons/bi"
+import "./index.css";
 
 
 function Assignments() {
-  const { courseId } = useParams();
-  const assignments = db.assignments;
-  const courseAssignments = assignments.filter(
-    (assignment) => assignment.course === courseId);
+  // const { courseId } = useParams();
+  // const assignments = db.assignments;
+  // const courseAssignments = assignments.filter(
+  //   (assignment) => assignment.course === courseId);
   return (
-    <div>
+  
+
+    <div className="wd-flex-grow-1">
+      <input placeholder="Search for Assignment"/>
+      <div className="wd-flex-row-container-1 wd-flex-grow-1">
+            <button class="btn1">
+              <AiOutlinePlus/>
+              Group</button> 
+            <button class="btn1 btn-danger">
+              <AiOutlinePlus/>
+                Assignment
+            </button>
+            <button class="btn1">
+            <BiDotsVerticalRounded/>
+            </button>
+      </div>      
+        <hr />
+     
+      <AssignmentList/>
+
+    {/* <div>
       <h3>Assignments for course {courseId}</h3>
       <div className="list-group">
         {courseAssignments.map((assignment) => (
@@ -21,6 +46,7 @@ function Assignments() {
           </Link>
         ))}
       </div>
+    </div> */}
     </div>
   );
 }
