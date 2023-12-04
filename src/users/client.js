e65b9651cb78a34029755acafac1a3c41b1d628b
hrishikasamani <samani.hr@northeastern.edu>
2023-12-04 00:49:04 -0500

    wwdc

--- src/users/client.js
+++ src/users/client.js
@@ -3,7 +3,7 @@ const request = axios.create({
     withCredentials: true,
   });  
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
-export const USERS_API = 'http://localhost:4000/api/users';
+export const USERS_API = 'https://kanbas-node-server-app-mongo-bb6o.onrender.com/api/users';
 // export const USERS_API = `${BASE_API}/api/users`;
 export const signin = async (credentials) => {
   const response = await request.post( `${USERS_API}/signin`, credentials );
